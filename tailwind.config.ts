5424687681d7915d031cb1976f863cfc5dc20ae8
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-15 19:33:57 -0300

    feat(colors): applied colors to project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './src/app/*.{js,ts,jsx,tsx}',
-        './components/*.{js,ts,jsx,tsx}',
+        './src/app/**/*.{js,ts,jsx,tsx}',
+        './src/components/**/*.{js,ts,jsx,tsx}',
     ],
-    theme: {
-        extend: {
-            fontFamily: {
-                'inter': ['Inter', 'sans-serif'],
-                'sans': ['sans-serif'],
-            }
-        }
-    },
+    darkMode: 'class',
+    theme: {},
     plugins: [],
-}
+};
