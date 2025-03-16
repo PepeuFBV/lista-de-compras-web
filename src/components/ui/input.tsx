93150e15f1079e4de6a5b159e1e2fab700bd5491
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-15 22:48:48 -0300

    feat(input): made input component

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,27 @@
+import { cn } from '@/lib/utils'
+import { InputHTMLAttributes } from 'react'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label?: string
+    className?: string
+}
+const Input: React.FC<InputProps> = ({ label, className, id, ...props }) => {
+    return (
+        <div className={cn('group flex flex-col gap-[8px]', className)}>
+            {label && (
+                <label className="text-secondary transition-colors group-focus-within:text-purple-light">{label}</label>
+            )}
+            <input
+                data-slot="input"
+                className={cn(
+                    'placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground border-border flex h-[40px] w-full min-w-0 rounded-md border bg-muted px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+                    'aria-invalid:ring-destructive/20 aria-invalid:border-destructive',
+                    'focus:border-purple-light'
+                )}
+                {...props}
+            />
+        </div>
+    )
+}
+
+export { Input }
