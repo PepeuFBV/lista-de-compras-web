fbc5b83a6d89f93d8b3804b89c51248cd61c5e6b
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-17 10:23:58 -0300

    fix(select): fixed select's background
    Added a background style when the item is checked and on the user's hover

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -111,8 +111,9 @@ function SelectItem({
         <SelectPrimitive.Item
             data-slot='select-item'
             className={cn(
-                'focus:bg-background-secondary [&_svg:not([class*="text - "])]:text-muted-foreground relative flex w-full cursor-pointer items-center gap-2 py-2 pr-8 pl-2 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*="size - "])]:size-4 *:[span]:last:flex *:[span]:last:items-center *:[span]:last:gap-2',
+                'focus:bg-border hover:bg-border [&_svg:not([class*="text - "])]:text-muted-foreground relative flex w-full cursor-pointer items-center gap-2 py-2 pr-8 pl-2 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*="size - "])]:size-4 *:[span]:last:flex *:[span]:last:items-center *:[span]:last:gap-2',
                 'border-b border-border last:border-none',
+                'data-[state=checked]:bg-border',
                 className
             )}
             {...props}
