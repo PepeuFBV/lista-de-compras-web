ed485beec50ca3b4afaa80eb34735b33849f28f7
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-17 17:07:48 -0300

    feat(axios): added axios instance to handle api

---
+++ src/lib/axios.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const apiUrl: string = process.env.NEXT_PUBLIC_API_URL as string
+
+const backendInstance = axios.create({
+    baseURL: apiUrl,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization'
+    }
+})
+
+export { backendInstance }
