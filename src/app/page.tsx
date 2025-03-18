47b561fa7478ea8f3af87db35a19b950f3734279
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-17 23:36:46 -0300

    fix(main-page): fixed main page flex styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,23 +21,27 @@ export default function Home() {
                 <div className='w-full max-w-[720px] flex flex-col items-start'>
                     <h1 className='text-2xl font-bold tracking-wide'>Lista de Compras</h1>
                 </div>
-                <div className='w-full max-w-[720px] flex items-center gap-2'>
-                    <Input className='w-full' />
-                    <UnitDropdown />
-                    <div className='flex flex-col gap-2'>
-                        <label className='text-xs text-secondary'>Categoria</label>
-                        <Select>
-                            <SelectTrigger className='w-[200px]'>
-                                <SelectValue placeholder='Selecione' />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {itemTypes.map((itemType, index) => (
-                                    <SelectItem key={index} value={itemType.name}>{itemType.name}</SelectItem>
-                                ))}
-                            </SelectContent>
-                        </Select>
+                <div className='w-full max-w-[720px] flex flex-col md:flex-row items-center gap-2'>
+                    <div className='w-full'>
+                        <Input className='w-full' />
                     </div>
-                    <AddButton className='mt-6' />
+                    <div className='max-w-[720px] md:max-w-none flex gap-2'>
+                        <UnitDropdown />
+                        <div className='flex flex-col gap-2'>
+                            <label className='text-xs text-secondary'>Categoria</label>
+                            <Select>
+                                <SelectTrigger className='w-[160px] md:w-[200px]'>
+                                    <SelectValue placeholder='Selecione' />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {itemTypes.map((itemType, index) => (
+                                        <SelectItem key={index} value={itemType.name}>{itemType.name}</SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <AddButton className='mt-6' />
+                    </div>
                 </div>
                 <Items className='w-full max-w-[720px]' />
             </div>
