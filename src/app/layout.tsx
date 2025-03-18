30776bed5531ad10d799bb760936c88b7ec8a603
Pedro <pedrofigueiraboaviagem@gmail.com>
2025-03-18 20:13:37 -0300

    feat(toaster): added sonner toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 
 import ContextProvider from '@/context/context'
 import Image from 'next/image'
+import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({
     variable: '--font-inter',
@@ -33,6 +34,7 @@ export default function RootLayout({
                         <Image className='' src='/background.png' alt='background' width={1920} height={165} quality={100} style={{ width: '100%', height: '185px', objectFit: 'cover' }} priority loading='eager' />
                     </div>
                     {children}
+                    <Toaster />
                 </ContextProvider>
             </body>
         </html>
--- src/components/core/add-item.tsx
+++ src/components/core/add-item.tsx
@@ -14,6 +14,7 @@ import {
 import { itemTypes } from '@/data/item-types'
 import { AddButton } from '@/components/core/add-button'
 import { createItem } from '@/services/backend'
+import { toast } from 'sonner'
 import { Item, ItemRequest } from '@/types/item'
 import { cn } from '@/lib/utils'
 
@@ -86,6 +87,7 @@ const AddItem: React.FC<AddItemProps> = ({ className }) => {
         try {
             const item: Item = await createItem(newItem)
             setItems([...items, item])
+            toast.success('Item adicionado com sucesso.')
         } catch (error) {
             console.error(error)
         }
